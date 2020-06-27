8a15ffc614bda844ce188b662c3ced01141dfacf
Eran Mazuz <mazuzeran@gmail.com>
2020-06-27 14:14:14 +0300

    Create plugin template

---
+++ Src/Plugin/Iplugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin
+{
+    public interface Iplugin
+    {
+        string Name { get; }
+
+        string Description { get; }
+
+        string Cover { get; }
+    }
+}
